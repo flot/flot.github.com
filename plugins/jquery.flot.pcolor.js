29fb722f6f7a8823127432c7459aed819139652d
Vincent Paeder <vincent.paeder@tuwien.ac.at>
2015-07-24 16:45:34 +0200

    performance hacks

--- plugins/jquery.flot.pcolor.js
+++ plugins/jquery.flot.pcolor.js
@@ -23,7 +23,7 @@
 				ocnv.width = 16384;
 				ocnv.height = 1;
 				var octx = ocnv.getContext("2d");
-				var grd = octx.createLinearGradient(0,0,16384,0);
+				var grd = octx.createLinearGradient(0,0,16385,0);
 				if (opt.series.pcolor.colormap==undefined) {
 					var colormap = [[0,"#0000ff"],[0.5,"#ffffff"],[1,"#ff0000"]];
 				} else {
@@ -33,8 +33,8 @@
 					grd.addColorStop(colormap[n][0],colormap[n][1]);
 				}
 				octx.fillStyle = grd;
-				octx.fillRect(0,0,16384,1);
-				plot.colormap = octx.getImageData(0,0,16384,1);
+				octx.fillRect(0,0,16385,1);
+				plot.colormap = octx.getImageData(0,0,16385,1);
 				// creates off-screen canvas and context for plot rendering
 				plot.ocnv = document.createElement("canvas");
 				plot.octx = plot.ocnv.getContext("2d");
@@ -56,9 +56,9 @@
 					var new_map_x = new Array(h);
 					var x_min = Math.min.apply(null, serie.data[0]);
 					var x_max = Math.max.apply(null, serie.data[0]);
-					for (var i=0; i<h; i++) {
+					for (var i = 0; i !== h; i++) {
 						new_map_x[i] = new Array(w);
-						for (var j=0; j<w; j++) {
+						for (var j = 0; j !== w; j++) {
 							new_map_x[i][j] = serie.data[0][j];
 						}
 					}
@@ -66,7 +66,7 @@
 				} else {
 					var x_min = 1e32;
 					var x_max = -1e32; 
-					for (var i=0; i<h; i++) {
+					for (var i = 0; i !== h; i++) {
 						x_min = Math.min(x_min, Math.min.apply(null, serie.data[0][i]));
 						x_max = Math.max(x_max, Math.max.apply(null, serie.data[0][i]));
 					}
@@ -75,9 +75,9 @@
 					var new_map_y = new Array(h);
 					var y_min = Math.min.apply(null, serie.data[1]);
 					var y_max = Math.max.apply(null, serie.data[1]);
-					for (var i=0; i<h; i++) {
+					for (var i = 0; i !== h; i++) {
 						new_map_y[i] = new Array(w);
-						for (var j=0; j<w; j++) {
+						for (var j = 0; j !== w; j++) {
 							new_map_y[i][j] = serie.data[1][i];
 						}
 					}
@@ -85,7 +85,7 @@
 				} else {
 					var y_min = 1e32;
 					var y_max = -1e32; 
-					for (var i=0; i<h; i++) {
+					for (var i = 0; i !== h; i++) {
 						y_min = Math.min(y_min, Math.min.apply(null, serie.data[1][i]));
 						y_max = Math.max(y_max, Math.max.apply(null, serie.data[1][i]));
 					}
@@ -97,7 +97,7 @@
 				// finds data range
 				var c_min = 1e32;
 				var c_max = -1e32;
-                for (var i = 0; i < h; i++) {
+                for (var i = 0; i !== h; i++) {
 					c_min = Math.min(c_min, Math.min.apply(null, serie.data[2][i]));
 					c_max = Math.max(c_max, Math.max.apply(null, serie.data[2][i]));
 				}
@@ -126,8 +126,8 @@
 				var sh = ch/ph*(ph-offset.top-offset.bottom)/h*hscale;
 				
 				// builds array of pixels
-                for (var i = 0; i < h; i++) {
-	                for (var j = 0; j < w; j++) {
+                for (var i = 0; i !== h; i++) {
+	                for (var j = 0; j !== w; j++) {
 						if (serie.data[0][i][j]>=x_min && serie.data[0][i][j]<=x_max &&
 							serie.data[1][i][j]>=y_min && serie.data[1][i][j]<=y_max) {
 								var px = (w-1)*(serie.data[0][i][j]-x_min)/dx;
@@ -140,7 +140,7 @@
 								var idxpm = (pyp*w+pxm)*4;
 								var idxmp = (pym*w+pxp)*4;
 								var idxpp = (pyp*w+pxp)*4;
-								var v = 4*parseInt((serie.data[2][i][j]-c_min)/dc*16383);
+								var v = 4*parseInt(((serie.data[2][i][j]-c_min)/dc)*16384);
 								var r = plot.colormap.data[v];
 								var g = plot.colormap.data[v+1];
 								var b = plot.colormap.data[v+2];
